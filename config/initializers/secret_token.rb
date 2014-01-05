9eb6d01c5f9e739f2f9798e91dbb8e41da0b4ad1
Jason Fletcher <jfletch@gmail.com>
2014-01-04 19:57:04 -0500

    - Initial commit of default Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jeopardy::Application.config.secret_key_base = 'cddbe1bcfa3b6d6912debbb827d7c55a310d4b50387132b09cabf8a032d5e1f2c31959a7240fc96c916ad5ff6aa9a1c456a87c0174e1b0bdb7ad86524f1ba93b'
